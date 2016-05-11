f617089b16470486a66276107e8fbb21887c76bd
Nathan Glover <nathan@glovers.id.au>
2016-05-11 12:41:20 +0800

    First draft

---
+++ src/uni/conf.js
@@ -0,0 +1,21 @@
+/*
+ * NB: for images, if you pass in a relative path ('assets/logo.png'), they will be automatically embedded (base64). If you pass in a URL, it will stay as it is.
+ */
+
+module.exports = [
+  {
+    id: 'nathan-glover-uni', // Will be appended to the built templates.
+    signature: 'Regards,',
+    name: 'Nathan Glover',
+    welcome: 'Hi,',
+    introParagraph: 'I hope you are having a great day.<br/>',
+    contactMain: '<a href="tel:+61430319001"><span>+61430319001</span></a>', // Wrap phone numbers like this to prevent iOS mail from making them blue.
+    contactMail: 'nathan.glover@student.curtin.edu.au',
+    address: '75 Cargill Street, Victoria Park 6100',
+    logoUrl: 'http://t04glovern.github.io/images/about-me/nathan-dp.png',
+    logoAlt: 'nathan-glover logo',
+    website: 'http://nathanglover.com',
+    blog: 'http://t04glovern.github.io/',
+    studentid: '15164755'
+  }
+]
